ccc5d72eca789e6a853fdf41bef3afd5df7f596b
Gwithjoy <bangbabo131@gmail.com>
2022-08-03 10:32:42 +0900

    final

---
+++ kafka/google_review_consumer (1).py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import time
+import os
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from bs4 import BeautifulSoup
+import requests
+import json, csv
+import traceback
+from kafka import KafkaConsumer
+from numpy import result_type
+from pymongo import MongoClient
+import pymongo
+
+
+USER = "admin2"
+PWD = "1111"
+HOST = "ec2-54-248-99-240.ap-northeast-1.compute.amazonaws.com"
+PORT = "27017"
+client = pymongo.MongoClient(f"mongodb://{USER}:{PWD}@{HOST}:{PORT}")
+db = client['google']
+collection = db['google_review_kafka']
+BROKERS = ["localhost:9092"]
+TOPIC_NAME = "google_games_topic"
+consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers = BROKERS)
+
+while True:
+    for message in consumer:
+        doc = json.loads(message.value.decode())
+        collection.insert_one(doc)
+        print(doc)
